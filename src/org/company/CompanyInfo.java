20c02bb8bcce9e2c321703033d1d6c5d45692481
avipriyankaa216 <priyankaaoct21@gmail.com>
2022-09-21 15:43:35 +0530

    verify the code

---
+++ src/org/company/CompanyInfo.java
@@ -0,0 +1,23 @@
+package org.company;
+
+public class CompanyInfo {
+private void companyName() {
+		// TODO Auto-generated method stub
+System.out.println("The name of the company is Harman");
+	}
+private void companyId() {
+	// TODO Auto-generated method stub
+	System.out.println("companyId is 87546 ");
+
+}
+private void companyAddress() {
+	// TODO Auto-generated method stub
+System.out.println("The company address is hennur ,Bangalore");
+}
+public static void main(String[] args) {
+	CompanyInfo c=new CompanyInfo();
+	c.companyName();
+	c.companyId();
+	c.companyAddress();
+}
+}
